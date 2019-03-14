b44c49a50064ee9dfce71ea5973271e0440fb456
TUR7L3 <turtleplusplus@gmail.com>
2019-03-14 11:13:19 -0700

    Added reset button. Can take user input to change the density of the grid

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-var gridSize = 16; 
+var gridSize = 32; 
 var grid = document.getElementById('grid');
 
 var temp;
+
+const resetBtn = document.createElement('BUTTON');
+resetBtn.appendChild(document.createTextNode("CLEAR"));
+
+const input = document.createElement('input');
+input.setAttribute('type','text');
+
+
+function initEAS(){
+    resetBtn.addEventListener("click", rebuildGrid);
+    document.body.insertBefore(resetBtn, document.body.firstChild);
+    document.body.insertBefore(input, document.body.firstChild);
+    initGrid();
+}
 
 function initGrid(){
     
@@ -9,6 +23,11 @@ function initGrid(){
     for(i = 0; i < area; i++){
         temp = document.createElement('div');
         temp.className = 'gridNode';
+
+        grid.style.setProperty('--gridSize', gridSize);
+
+        input.setAttribute('value','NEW GRID SIZE');
+
         temp.addEventListener('mouseover', onHover);
         document.getElementById('grid').appendChild(temp);
         
@@ -20,15 +39,19 @@ function onHover(e){
 }
 
 function rebuildGrid(){
-    
-    
-    
+    let parsedInput = parseInt(input.value);
+    if(parsedInput){
+        gridSize = parsedInput;
+    }
+    console.log("rebuilding grid");
     destroyGrid();
     initGrid();
 }
 
 function destroyGrid(){
-
+    while(grid.firstChild){
+        grid.removeChild(grid.firstChild);
+    }
 }
 
-initGrid();
+initEAS();
